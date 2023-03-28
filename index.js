3881268e5ade695f8c1b65392f76502086d553f0
chamsou-Br <jc_berkane@esi.dz>
2023-03-28 15:12:05 +0100

    home page

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const app = express();
+const PORT = 8000;
+
+app.listen(PORT,()=>{
+    console.log("success")
+})
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.render('home', { name: 'World'  , data : [ 
+        [0,1,2,3,5,6,7,8,9],
+        [0,1,2,3,5,6,7,8,9],
+        [0,1,2,3,5,6,7,8,9]
+    ]});
+  });
