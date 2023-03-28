4ade1802c22ba5927c99025b11ae518c739ee0a5
chamsou-Br <jc_berkane@esi.dz>
2023-03-28 18:38:13 +0100

    detail page

--- index.js
+++ index.js
@@ -16,4 +16,8 @@ app.get('/', (req, res) => {
         [0,1,2,3,5,6,7,8,9],
         [0,1,2,3,5,6,7,8,9]
     ]});
-  });
+});
+
+app.get("/details/:id",(req , res) => {
+    res.render("details");
+})
