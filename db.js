b18a8b9f7d679f3f8bf603beaf98c911f6298e3f
chamsou-Br <jc_berkane@esi.dz>
2023-04-15 23:47:33 +0100

    add postgress host

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 const Pool = require("pg").Pool
 
 const pool = new Pool({
-    user : "postgres",
-    password : "2002",
-    database : "Ecommerce",
-    host : "localhost"
+    user : "ecommerceclothes_salah",
+    password : "salah2001",
+    database : "ecommerceclothes_database",
+    host : "postgresql-ecommerceclothes.alwaysdata.net"
 })
 
 module.exports = pool;
